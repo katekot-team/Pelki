0163e150c5f1637157ffd154d216e0e4f87cdbb3
SichTM <maxdlaclass@gmail.com>
2023-08-01 21:25:37 +0300

    IDamageable

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+using NaughtyAttributes;
+using Pelki.Interfaces;
+using UnityEngine;
+
+namespace Pelki.Player
+{
+    public class Player : MonoBehaviour, IDamageable
+    {
+        [SerializeField, MinValue(0)] private int maxHealth;
+        public int Health { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 1)
+            {
+                Death();
+            }
+            Debug.Log("Current health is " + Health);
+        }
+        
+        private void Start()
+        {
+            Health = maxHealth;
+        }
+        
+        private void Death()
+        {
+            
+        }
+    }
+}
